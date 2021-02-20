3fc0e7445cd7fa810222bc67abd23d451afb79c5
liuenci <liuenci@cvte.com>
2021-02-20 11:51:19 +0800

    [seata-dubbo] 残次版本，跑不起来

---
+++ seata-order/src/main/java/com/cier/seata/order/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.cier.seata.order.controller;
+
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.cier.seata.order.api.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    private Logger logger = LoggerFactory.getLogger(OrderController.class);
+
+    @Reference
+    private OrderService orderService;
+
+    @PostMapping("/create")
+    public Integer createOrder(@RequestParam("userId") Long userId,
+                               @RequestParam("productId") Long productId,
+                               @RequestParam("price") Integer price) throws Exception {
+        logger.info("[createOrder] 收到下单请求,用户:{}, 商品:{}, 价格:{}", userId, productId, price);
+        return orderService.createOrder(userId, productId, price);
+    }
+
+}
